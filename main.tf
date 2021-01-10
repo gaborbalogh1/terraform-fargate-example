de071429884386ead26f0da99e43bef36a1a9c52
Gabor Balogh <gabor.balogh1@gmail.com>
2021-01-10 16:47:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  region     = var.region
-  access_key = var.AWS_SECRET_ACCESS_KEY
-  secret_key = var.AWS_ACCESS_KEY_ID
-}
-
-
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.1.0.0/16"
