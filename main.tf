a4fb501036de2099bbe451aad7f0e8763289d91c
Gabor Balogh <gabor.balogh1@gmail.com>
2021-01-10 13:17:26 +0000

    Update main.tf
    add ecs to main and create a vpc to use.

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+provider "aws" {
+  region     = AWS_DEFAULT_REGION
+  access_key = AWS_SECRET_ACCESS_KEY
+  secret_key = AWS_ACCESS_KEY_ID
+}
 
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.1.0.0/16"
+}
+
+#crete ecs
+resource "aws_ecs_cluster" "Gabaltech_ECS_Cluster" {
+  name = "Gabaltech_ECS_Cluster"
+  capacity_providers = FARGATE
+}
